fb742b253e239b9edcef1b55a8b791775e4d1447
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-10-25 00:26:32 +0900

    delete : いらないファイルを削除

--- backend/main.py
+++ backend/main.py
@@ -6,18 +6,6 @@ from sqlalchemy import create_engine
 
 app = FastAPI()
 
-dialect = "postgresql"
-driver = "default"
-username = "psql"
-password = "psql"
-host = "localhost"
-port = "5432"
-database = "psql"
-charset_type = "utf8"
-engine = create_engine(
-    f"{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}?charset={charset_type}"
-)
-
 
 @app.get("/")
 def read_root():
