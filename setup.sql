4458146df60ab61fff3176639f3cab0792391476
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-10-25 19:40:49 +0900

    add : setup.sqlのis_compile->is_compiledに修正

--- setup.sql
+++ setup.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,                -- id列: 自動増分する整数型の主キー
     self_introduction TEXT,               -- self_introduction列: TEXT型
     params_json JSON,                     -- params_json列: JSON型
-    is_compile BOOLEAN DEFAULT FALSE      -- is_compile列: BOOLEAN型、デフォルト値はFALSE
+    is_compiled BOOLEAN DEFAULT FALSE      -- is_compiled列: BOOLEAN型、デフォルト値はFALSE
 );
