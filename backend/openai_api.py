0a0977f15d376f2439ba80db07f73b5c728116c5
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-10-26 08:41:35 +0900

    add : openaiのクライアント追加

--- backend/openai_api.py
+++ backend/openai_api.py
@@ -1,2 +1,49 @@
-def convert2params_json(self_introduction: str):
-    return {"interests": 80, "skills": 85, "approach": 70}
+from openai import OpenAI
+from dotenv import load_dotenv
+import os, sys
+from os.path import join, dirname
+import json
+
+load_dotenv()
+
+
+def isJsonFormat(string: str):
+    try:
+        json.loads(string)
+    except json.JSONDecodeError as e:
+        print(sys.exc_info())
+        print(e)
+        return False
+    # 以下の例外でも捕まえるので注意
+    except ValueError as e:
+        print(sys.exc_info())
+        print(e)
+        return False
+    except Exception as e:
+        print(sys.exc_info())
+        print(e)
+        return False
+    return True
+
+
+def convert2params_json(self_introduction: str, client: OpenAI):
+    with open("./script.txt") as f:
+        script = f.read()
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {
+                    "role": "user",
+                    "content": f"""{script}```{self_introduction}```""",
+                },
+            ],
+        )
+    result: str = completion.choices[0].message.content
+    result = result.replace("\n", "")  # 改行削除
+    print(result)
+    if isJsonFormat(result):
+        result_json = json.loads(result)
+        return result_json
+    else:
+        return {"Error ChatGPT response is not json format": result}
